1c6906790ace5bd9750b0e9ca1d2025e3c864c3b
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-05 11:16:37 +0800

    Create Client route

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createRoutesFromElements, createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
+import { Toaster } from 'react-hot-toast';
 import './index.css';
 import Root from './routes/root';
 import ErrorPage from './error-page';
 import SignIn from './routes/sign-in';
 import SignUp from './routes/sign-up';
-import { Toaster } from 'react-hot-toast';
+import Client from './routes/client';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -15,6 +16,7 @@ const router = createBrowserRouter(
       <Route path="/" element={<Root />} errorElement={<ErrorPage />} />
       <Route path="/sign_in" element={<SignIn />} />
       <Route path="/sign_up" element={<SignUp />} />
+      <Route path="/client" element={<Client />} />
     </>
   )
 );
