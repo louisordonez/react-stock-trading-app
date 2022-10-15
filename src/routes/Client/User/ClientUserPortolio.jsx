4d43282999cd20b538b43f3be33b872444b1c131
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-15 12:57:08 +0800

    Update user portofolio design

--- src/routes/Client/User/ClientUserPortolio.jsx
+++ src/routes/Client/User/ClientUserPortolio.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import { Title, Text, Paper, Group, ScrollArea, Input, Stack, Button, Image } from '@mantine/core';
 
 const ClientUserPortolio = () => {
-  return <div>ClientUserPortolio</div>;
+  return (
+    <>
+      <Title pl={16}>Portfolio</Title>
+      <Group px={16} pt={16} grow>
+        <Paper p="xl" withBorder>
+          <ScrollArea>
+            <Group>
+              <div style={{ width: 180 }}>
+                <Image
+                  radius="md"
+                  src="https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                />
+              </div>
+              <Stack>
+                <Text weight={700}>MSFT</Text>
+                <Text>Microsoft</Text>
+                <Text>$100.00</Text>
+                <Text>Owned: 10</Text>
+              </Stack>
+            </Group>
+          </ScrollArea>
+        </Paper>
+      </Group>
+    </>
+  );
 };
 
 export default ClientUserPortolio;
