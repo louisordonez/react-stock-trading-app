7b68a1623aed9c7a143c1db3309342be973ed772
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-19 19:23:11 +0800

    Change base url to production

--- src/services/utilities/axios.js
+++ src/services/utilities/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const StockTradingRef = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://stock-trading-app-ha2z.onrender.com',
 });
 
 export const axiosGet = async (endpoint, headers) => {
