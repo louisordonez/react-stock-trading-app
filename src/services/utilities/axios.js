2c462314dded1252d1ddd5659f2a7f6790b4c29a
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-20 00:34:54 +0800

    Update axios.js

--- src/services/utilities/axios.js
+++ src/services/utilities/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const StockTradingRef = axios.create({
   // baseURL: 'http://localhost:3000/api/v1',
-  baseURL: 'https://stock-trading-app-ha2z.onrender.com/',
+  baseURL: 'https://stock-trading-app-ha2z.onrender.com/api/v1',
 });
 
 export const axiosGet = async (endpoint, headers) => {
