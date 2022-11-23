53d33339a6768494a0cba8b8471c9d2a1987edaa
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-11-24 01:20:23 +0800

    Update links.js

--- src/services/constants/links.js
+++ src/services/constants/links.js
@@ -1,6 +1,6 @@
-export const SIGN_IN_LINK = '/sign_in';
-export const SIGN_UP_LINK = '/sign_up';
-export const VERIFY_EMAIL_LINK = '/verify_email';
+export const SIGN_IN_LINK = '/sign-in';
+export const SIGN_UP_LINK = '/sign-up';
+export const VERIFY_EMAIL_LINK = '/verify-email';
 
 export const CLIENT_DASHBOARD_LINK = '/client/dashboard';
 export const CLIENT_MARKET_LINK = '/client/market';
