c80a898e6197801c6ecaf06d77f518d004a907ab
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-11-23 23:20:20 +0800

    Update useRedirect.js

--- src/services/utilities/useRedirect.js
+++ src/services/utilities/useRedirect.js
@@ -1,58 +1,56 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { SHOW_USER_ENDPOINT } from '../constants/usersEndpoints';
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { SHOW_USER_ENDPOINT } from '../constants/usersEndpoints'
 import {
   SIGN_IN_LINK,
   SIGN_UP_LINK,
   VERIFY_EMAIL_LINK,
   CLIENT_DASHBOARD_LINK,
-} from '../constants/links';
-import { accessTokenCookie } from '../constants/cookies';
-import { isLoggedIn } from './isLoggedIn';
-import { axiosGet } from './axios';
-import { getCookie } from './cookie';
+} from '../constants/links'
+import { accessTokenCookie } from '../constants/cookies'
+import { isLoggedIn } from './isLoggedIn'
+import { axiosGet } from './axios'
+import { getCookie } from './cookie'
 
 export const useRedirect = () => {
-  const navigate = useNavigate();
-  const accessToken = getCookie(accessTokenCookie);
+  const navigate = useNavigate()
+  const accessToken = getCookie(accessTokenCookie)
   const headers = {
     'Access-Control-Allow-Origin': '*',
     Authorization: accessToken,
-  };
-  let urlPath = window.location.pathname.split('/');
+  }
+  let urlPath = window.location.pathname.split('/')
 
-  urlPath = `/${urlPath[1]}`;
+  urlPath = `/${urlPath[1]}`
 
   useEffect(() => {
-    // isLoggedIn().then((response) => {
-      if (isLoggedIn()) {
-        axiosGet(SHOW_USER_ENDPOINT, headers).then((response) => {
-          if (!response.data.email_verified) {
-            navigate(VERIFY_EMAIL_LINK);
-          }
-        });
+    if (isLoggedIn()) {
+      axiosGet(SHOW_USER_ENDPOINT, headers).then((response) => {
+        if (!response.data.email_verified) {
+          navigate(VERIFY_EMAIL_LINK)
+        }
+      })
 
-        switch (urlPath) {
-          case '/':
-            navigate(CLIENT_DASHBOARD_LINK);
-            break;
-          case SIGN_IN_LINK:
-          case SIGN_UP_LINK:
-            navigate(CLIENT_DASHBOARD_LINK);
-        }
-      } else {
-        switch (urlPath) {
-          case '/':
-            navigate('/');
-            break;
-          case SIGN_UP_LINK:
-            navigate(SIGN_UP_LINK);
-            break;
-          default:
-            navigate(SIGN_IN_LINK);
-            break;
-        }
-     // }
-    });
-  }, []);
-};
+      switch (urlPath) {
+        case '/':
+          navigate(CLIENT_DASHBOARD_LINK)
+          break
+        case SIGN_IN_LINK:
+        case SIGN_UP_LINK:
+          navigate(CLIENT_DASHBOARD_LINK)
+      }
+    } else {
+      switch (urlPath) {
+        case '/':
+          navigate('/')
+          break
+        case SIGN_UP_LINK:
+          navigate(SIGN_UP_LINK)
+          break
+        default:
+          navigate(SIGN_IN_LINK)
+          break
+      }
+    }
+  }, [])
+}
